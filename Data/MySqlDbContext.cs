a213778101b1dba39a930f759593d608761e6582
snowitty <snowitty@163.com>
2023-09-23 08:52:06 +0800

    💩💩DbContext

---
+++ Data/MySqlDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace inkyore_netcore.Data
+{
+    public class MySqlDbContext : DbContext
+    {
+        public MySqlDbContext(DbContextOptions<MySqlDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Data/PostgreSqlDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace inkyore_netcore.Data
+{
+    public class PostgreSqlDbContext : DbContext
+    {
+        public PostgreSqlDbContext(DbContextOptions<PostgreSqlDbContext> options) : base(options) { }
+    }
+}
